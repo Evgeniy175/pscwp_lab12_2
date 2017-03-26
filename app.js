5bfae6a903f60c142c4cc0d65a96267087d36e60
Evgeniy175 <ryabchenko.evgeniy.175@gmail.com>
2017-03-26 21:26:51 +0300

    edited

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = Express();
 
 // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- //
 
-app.use('/', (req, res) => { res.end('Hello World!') });
+app.use('/', (req, res) => { res.end('Hello azaza World!') });
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Server running...');
