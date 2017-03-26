000e48afd804aa23481b41cf10b3d46fa9578708
Evgeniy175 <ryabchenko.evgeniy.175@gmail.com>
2017-03-26 21:16:49 +0300

    rdy

---
+++ app.js
@@ -0,0 +1,13 @@
+const Express = require('express');
+
+// -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- //
+
+const app = Express();
+
+// -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- // -- //
+
+app.use('/', (req, res) => { res.end('Hello World!') });
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server running...');
+});
